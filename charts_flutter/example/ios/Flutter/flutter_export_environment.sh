ce989ef0a42b5773a9cffe182495b01a668cf050
Oliver Kocsis <okocsis88@gmail.com>
2021-02-04 10:19:38 +0100

    ?

--- charts_flutter/example/ios/Flutter/flutter_export_environment.sh
+++ charts_flutter/example/ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/endre/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/endre/Dropbox/telenetdoktor/Privatdoktor-flutter/charts_flutter-0.8.1/example"
+export "FLUTTER_ROOT=/usr/local/Caskroom/flutter/1.22.4/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/oliver/Privatdoktor/mobil-kliens/self_managed_dependencies/charts/charts_flutter/example"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/endre/development/flutter/bin/cache/artifacts/engine/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/usr/local/Caskroom/flutter/1.22.4/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
